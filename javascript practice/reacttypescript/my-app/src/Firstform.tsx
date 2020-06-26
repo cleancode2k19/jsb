91f5898ade50e498d8b905397129c234fef04ee3
cleancode2k19 <49688548+cleancode2k19@users.noreply.github.com>
2020-06-26 15:40:19 +1000

    added functional component

--- javascript practice/reacttypescript/my-app/src/Firstform.tsx
+++ javascript practice/reacttypescript/my-app/src/Firstform.tsx
@@ -1,12 +1,46 @@
 import React, { useState } from "react";
-export default function Firstform(props) {
+function Firstform() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <>
-      <label>
-        Name:
-        <input type="text" />
-      </label>
-      <input type="submit" value="Submit" />
-    </>
+    <form>
+      <input
+        value={firstName}
+        onChange={e => setFirstName(e.target.value)}
+        placeholder="First name"
+        type="text"
+        name="firstName"
+        required
+      />
+      <input
+        value={lastName}
+        onChange={e => setLastName(e.target.value)}
+        placeholder="Last name"
+        type="text"
+        name="lastName"
+        required
+      />
+      <input
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+        placeholder="Email address"
+        type="email"
+        name="email"
+        required
+      />
+      <input
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+        placeholder="Password"
+        type="password"
+        name="password"
+        required
+      />
+      <button type="submit">Submit</button>
+      <div>{firstName}{lastName}{email}</div>
+    </form>
   );
 }
+export default Firstform;
